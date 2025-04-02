d3cc33e3dcdf0bf6efc126afbf458db091489f4b
minyou2675 <kymyy2675@gmail.com>
2025-04-02 20:15:52 +0900

    feat: 백엔드 프로젝트 생성

---
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/LpsearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lpsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpsearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpsearchBackendApplication.class, args);
+	}
+
+}
