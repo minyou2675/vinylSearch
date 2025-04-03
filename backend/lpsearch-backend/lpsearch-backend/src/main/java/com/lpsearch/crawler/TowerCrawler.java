73fab4241c1ddfa29af81a91224dc1f9e0b23373
minyou2675 <kymyy2675@gmail.com>
2025-04-03 23:18:00 +0900

    feat:jsoup 의존성 추가

--- backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/crawler/TowerCrawler.java
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/crawler/TowerCrawler.java
@@ -3,7 +3,7 @@ package com.lpsearch.crawler;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import jsoup.select.Elements;
+import org.jsoup.select.Elements;
 
 public class TowerRecordsCrawler {
 
