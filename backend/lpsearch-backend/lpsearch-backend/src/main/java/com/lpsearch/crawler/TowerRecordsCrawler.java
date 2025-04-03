52847ef2fd8ac569ac9e40329a21ac255e4400e6
minyou2675 <kymyy2675@gmail.com>
2025-04-03 23:43:38 +0900

    refactor:test코드 수정

--- backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/crawler/TowerRecordsCrawler.java
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/crawler/TowerRecordsCrawler.java
@@ -69,3 +69,4 @@ public class TowerRecordsCrawler {
 
         return results;
     }
+    }
--- backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/TowerRecordsCrawlerTest.java
+++ backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/TowerRecordsCrawlerTest.java
@@ -2,9 +2,12 @@ package com.lpsearch;
 
 import com.lpsearch.crawler.TowerRecordsCrawler;
 import com.lpsearch.dto.AlbumDto;
+import com.lpsearch.repository.AlbumRepository;
+import com.lpsearch.repository.AlbumStockRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
@@ -15,12 +18,12 @@ class TowerRecordsCrawlerTest {
 
     @Autowired
     private TowerRecordsCrawler towerCrawler;
-
+    
     @Test
     void testCrawl() {
         List<AlbumDto> result = towerCrawler.crawl("radiohead");
 
-        assertFalse(result.isEmpty());
+        assertFalse("크롤링 결과가 비어 있습니다!",result.isEmpty());
         result.forEach(album -> {
             System.out.println(album.getTitle() + " - " + album.getArtist());
         });
