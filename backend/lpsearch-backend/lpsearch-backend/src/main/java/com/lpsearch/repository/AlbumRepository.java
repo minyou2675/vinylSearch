8e8bcaa54d1299185d143abbf9342574460cd915
minyou2675 <kymyy2675@gmail.com>
2025-04-05 01:07:46 +0900

    refactor: AlbumRepository에findbytitleandartist 메소드 추가

--- backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/repository/AlbumRepository.java
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/repository/AlbumRepository.java
@@ -4,7 +4,9 @@ import com.lpsearch.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     List<Album> findByTitleContaining(String keyword);
+    Optional<Album> findByTitleAndArtist(String title, String artist);
 }
