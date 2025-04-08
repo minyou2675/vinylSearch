31203b4bff5d45a5f6a31cb47b8ebef7d8a36287
minyou2675 <kymyy2675@gmail.com>
2025-04-08 23:43:35 +0900

    feature:Implemented RedisSearchService

---
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/service/RedisSearchService.java
@@ -0,0 +1,36 @@
+package com.lpsearch.service;
+
+import com.lpsearch.domain.Album;
+import com.lpsearch.dto.AlbumDto;
+import jakarta.servlet.http.PushBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RedisSearchService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private final Duration CACHE_TTL = Duration.ofMinutes(30); //캐시 유효시간
+
+    public void saveSearchResult(String keyword, List<AlbumDto> result){
+        String key = makeKey(keyword);
+        redisTemplate.opsForValue().set(key, result, CACHE_TTL);
+    }
+    public List<AlbumDto> getSearchResult(String keyword){
+        String key = makeKey(keyword);
+        Object value = redisTemplate.opsForValue().get(key);
+        if(value != null && value instanceof List<?>){
+            return (List<AlbumDto>) value;
+        }
+        return null;
+    }
+    private String makeKey(String keyword){
+        return keyword.trim().toLowerCase();
+    }
+}
