135026a46d610aec4eadb452b86caba9448303e1
minyou2675 <kymyy2675@gmail.com>
2025-04-12 18:32:04 +0900

    feature:add EnableScheduling config

--- backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/LpsearchBackendApplication.java
+++ backend/lpsearch-backend/lpsearch-backend/src/main/java/com/lpsearch/LpsearchBackendApplication.java
@@ -2,8 +2,11 @@ package com.lpsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class LpsearchBackendApplication {
 
 	public static void main(String[] args) {
