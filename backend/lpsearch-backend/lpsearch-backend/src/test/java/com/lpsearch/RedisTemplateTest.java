796bb9b6407f4dd6a8049897001416acd1b2fa74
minyou2675 <kymyy2675@gmail.com>
2025-04-12 21:34:13 +0900

    test:redistTemplateTest 수정

--- backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/RedisTemplateTest.java
+++ backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/RedisTemplateTest.java
@@ -1,42 +1,20 @@
 package com.lpsearch;
 
-import com.lpsearch.crawler.TowerRecordsCrawler;
-import com.lpsearch.dto.AlbumDto;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.time.Duration;
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {RedisTemplateTest.TestRedisConfig.class})
 @SpringBootTest
-public class RedisTemplateTest {
+@ActiveProfiles("test")
+class RedisTemplateTest {
 
-    @Autowired
+    @MockBean
     private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     void RedisTest() {
-        // your test logic here
-    }
-
-    @TestConfiguration
-    static class TestRedisConfig {
-        @Bean
-        public RedisTemplate<String, Object> redisTemplate() {
-            RedisTemplate<String, Object> mock = mock(RedisTemplate.class);
-            // stubbing if needed
-            return mock;
-        }
+        // test logic
     }
 }
