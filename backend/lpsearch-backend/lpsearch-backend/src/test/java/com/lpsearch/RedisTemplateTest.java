8661798924a6757a273a525ff985446e54c06a0e
minyou2675 <kymyy2675@gmail.com>
2025-04-12 21:37:56 +0900

    test:redistTemplateTest 수정

--- backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/RedisTemplateTest.java
+++ backend/lpsearch-backend/lpsearch-backend/src/test/java/com/lpsearch/RedisTemplateTest.java
@@ -1,5 +1,6 @@
 package com.lpsearch;
 
+import com.lpsearch.service.RedisSearchService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,8 +14,12 @@ class RedisTemplateTest {
     @MockBean
     private RedisTemplate<String, Object> redisTemplate;
 
+    @MockBean
+    private RedisSearchService redisSearchService;
+
     @Test
     void RedisTest() {
-        // test logic
+        System.out.println("Redis 테스트 통과");
     }
 }
+
