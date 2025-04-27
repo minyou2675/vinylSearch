855743478c35511e6ef5a3eb57074fccd19de0a5
minyou2675 <kymyy2675@gmail.com>
2025-04-27 21:33:50 +0900

    feat:board용 DB 컨테이너 구축

--- backend/node-server/models/post.js
+++ backend/node-server/models/post.js
@@ -1,49 +1,46 @@
-const {Sequelize, DataTypes} = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 // .env 설정 로딩 -> db 접속
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: true, // sql 로그 확인
-    }
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+    logging: true, // sql 로그 확인
+  }
 );
 
-
 // 테이블 정의
-const Post = sequelize.define('Post', {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    views: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    
+const Post = sequelize.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  views: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
-  // 👇 이 부분을 테스트에서 제외할 수 있게 export만 하고,
-  // 실제 앱에서만 동기화 실행
-  module.exports = { Post, sequelize };
-
+// 👇 이 부분을 테스트에서 제외할 수 있게 export만 하고,
+// 실제 앱에서만 동기화 실행
+module.exports = { Post, sequelize };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,24 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Mainpage from "./pages/Mainpage";
+import MainPage from "./pages/MainPage";
 import Discover from "./pages/AlbumLayout/Discover";
 import DiscoverTest from "./pages/AlbumLayout/DiscoverTest";
 import Signup from "./pages/AuthLayout/SignupForm";
 import Login from "./pages/AuthLayout/LoginForm";
-import MyPage from "./pages/MyPage";
-import Favorite from "./pages/Favorite";
+import MyPage from "./pages/AuthLayout/MyPage";
+import Favorite from "./pages/AlbumLayout/Favorite";
 import BoardPost from "./pages/BoardLayout/BoardPost";
 import BoardPostWrite from "./pages/BoardLayout/BoardPostWrite";
 import BoardPostView from "./pages/BoardLayout/BoardPostView";
-import BatchDashboard from "./pages/BatchDashBoard";
-import BatchLogs from "./pages/BatchLogs";
+import BatchDashboard from "./pages/BatchLayout/BatchDashBoard";
+import BatchLogs from "./pages/BatchLayout/BatchLogs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Mainpage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/discover" element={<Discover />} />
         <Route path="/discover-test" element={<DiscoverTest />} />
         <Route path="/signup" element={<Signup />} />
